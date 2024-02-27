dbc56d2f096d108b001a7d878009926b3e1e935a
Scramper <33483324+Scramper@users.noreply.github.com>
2024-02-27 14:59:11 +0100

    build ready

--- ProjectManagementSystem.Core.Domain/Models/Sprint.cs
+++ ProjectManagementSystem.Core.Domain/Models/Sprint.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ProjectManagementSystem.Core.Domain.Models
 {
-    internal class Sprint
+    public class Sprint
     {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Task> Tasks { get; set; }
+
+        public void AddTask(Task task)
+        {
+            Tasks.Add(task);
+        }
     }
 }
--- ProjectManagementSystem.Core.Domain/Models/Task.cs
+++ ProjectManagementSystem.Core.Domain/Models/Task.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManagementSystem.Core.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
