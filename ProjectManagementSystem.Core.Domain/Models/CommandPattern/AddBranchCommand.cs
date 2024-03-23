dd74eb5f630dd927424b6b581eed192361dec744
OscarVerhagen <58423620+oscarverhagen@users.noreply.github.com>
2024-03-23 19:13:58 +0100

    ICommand

---
+++ ProjectManagementSystem.Core.Domain/Models/CommandPattern/AddBranchCommand.cs
@@ -0,0 +1,26 @@
+﻿using ProjectManagementSystem.Core.Domain.Interfaces;
+
+namespace ProjectManagementSystem.Core.Domain.Models.CommandPattern
+{
+    public class AddBranchCommand : ICommand
+    {
+        private Repository _repository;
+        private Branch _branch;
+
+        public AddBranchCommand(Repository repository, Branch branch)
+        {
+            _repository = repository;
+            _branch = branch;
+        }
+
+        public void Execute()
+        {
+            _repository.AddBranch(_branch);
+        }
+
+        public void Undo()
+        {
+            _repository.RemoveBranch(_branch);
+        }
+    }
+}
